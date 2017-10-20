2e5c6bfb68e272ea68bd760ab641f27572377cb8
Christopher Bargren <cbargren@gmail.com>
2017-10-20 11:29:27 -0700

    Adding configuration options for changing the default address and port

--- test.js
+++ test.js
@@ -3,15 +3,24 @@ const readline = require('readline');
 const ccm = require('./dist');
 
 const cluster = ccm.createCluster({
-  nodes: 2,
-  // purge: true,
+  nodes: 3,
+  purge: true,
+  jmxPort: 7101,
+  startAddress: '127.0.1.11',
   version: 3.8,
   // verbose: true,
 });
 
+const timeout = (t = 1000) => new Promise(resolve => setTimeout(resolve, t));
+
 cluster.initialize()
+  // .then(() => timeout())
   .then(() => cluster.populateNodes())
+  // .then(() => timeout())
+  .then(() => cluster.configureNodes())
+  // .then(() => timeout())
   .then(() => cluster.start())
+  // .then(() => timeout())
   .then(() => {
     const rl = readline.createInterface({
       input: process.stdin,
